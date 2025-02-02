57796539b7ccd499c9ab0e068a58efa2cbbb447e
gabrielmoreltorres <gabriel.morel@upr.edu>
2025-02-02 09:03:54 -0400

    Implemeneted Sun class

--- src/Classes/Sun.java
+++ src/Classes/Sun.java
@@ -8,7 +8,7 @@ package Classes;
 
  //Pull request test
 
-public class Sun /*TODO*/ {
+public class Sun implements CelestialBody /*TODO*/ {
 
 	/**
 	 * Static path to the image of the Sun.
@@ -16,13 +16,75 @@ public class Sun /*TODO*/ {
 	private static final String SUN_IMAGE = "./src/resources/Sun.jpg";
 
 
+	//TODO ADD THE FOLLOWING:
+	//PRIVATE FIELDS
+	private String name = "Sun";
+	private double mass;
+	private double radius;
+	private int[] coordinates;
 
-	/*TODO ADD THE FOLLOWING:
-	 * PRIVATE FIELDS, 
-	 * CONSTRUCTOR, 
-	 * GETTERS, 
-	 * SETTERS, 
-	 */
+	//CONSTRUCTOR,
+	public Sun(double sunMass, double sunRadius, int[] sunCoordinates){
+		if(sunMass <= 0 || sunRadius <= 0){
+			throw new IllegalArgumentException("Mass or Radius cannot be negative");
+		}
+		if(sunCoordinates.length < 2 || sunCoordinates.length > 2){
+			throw new IllegalArgumentException("Only provide X and Y coordinates");
+		}
+		mass = sunMass;
+		radius = sunRadius;
+		coordinates = sunCoordinates;
+	}
+
+	//GETTERS,
+	public double getMass(){
+		return this.mass;
+	}
+
+	public double getRadius(){
+		return this.radius;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public int[] getCoordinates(){
+		return this.coordinates;
+	}
+
+	//SETTERS
+	public void setMass(double mass){
+		if(mass > 0){
+			this.mass = mass;
+		}
+		else{
+			throw new IllegalArgumentException("Mass cannot be negative");
+		}
+	}
+
+	public void setRadius(double radius){
+		if(radius > 0){
+			this.radius = radius;
+		}
+		else{
+			throw new IllegalArgumentException("Radius cannot be negative")
+		}
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public void setCoordinates(int[] sunCoordinates){
+		if(sunCoordinates.length != 2){
+			throw new IllegalArgumentException("Only provide X and Y coordinates");
+		}
+		else{
+			this.coordinates = sunCoordinates;
+		}
+	}
+	
 
 
 	/**
